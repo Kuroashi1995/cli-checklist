3aaa491435f35b4e3a9cd3644b25c230a865ff43
Kuroashi1995 <kuroashi.prog@gmail.com>
2025-06-10 13:55:56 -0300

    file management testing

---
+++ test_file_management.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from file_management import get_todo_list
+from todo import Todo
+
+import os
+
+class TestFileManagement(TestCase):
+    def test_get_todos_no_file(self):
+        todos = get_todo_list("./example.txt")
+        self.assertEqual(todos, [])
+
+    def test_get_todos_file(self):
+        with open("./example.txt", "w") as example:
+            example.write("1:::Example:::False")
+        todos = get_todo_list("./example.txt")
+        self.assertEqual(todos, [Todo(1, "Example")])
+        os.remove("./example.txt")
+
+    def test_get_todos_wrong(self):
+        with open("./example.txt", "w") as example:
+            example.write("bad example")
+        todos = get_todo_list("./example.txt")
+        self.assertEqual(todos, [])
+        os.remove("./example.txt")
+
+
