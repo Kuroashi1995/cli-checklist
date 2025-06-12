63543d3f8b5d96a213723af03b1c812de7b45099
Kuroashi1995 <kuroashi.prog@gmail.com>
2025-06-12 18:33:24 -0300

    improve user experience and add edit option

--- main.py
+++ main.py
@@ -8,43 +8,59 @@ def main():
     while (True):
         try:
             print_todo_list(todo_list)
-            selection = input("1- Add Todo | 2- Complete Todo | 3- Delete Todo | 4- Clear All | 5- Clear Completed | 6- Order by Priority: > ")
-            if selection == "exit":
+            user_input = input("Enter new todo | Enter todo number | order | clear | exit: > ")
+            # Check for exit
+            if user_input == "exit":
                 break
-            elif selection != "":
-                match(int(selection)):
+            # Check for order
+            elif user_input == "order":
+                def filterer(todo: Todo) -> int | float:
+                    if todo.completed:
+                        return float("inf")
+                    else:
+                        return todo.priority
+                todo_list.sort(key=filterer)
+            #Check for clear
+            elif user_input == "clear":
+                option = int(input("1. Clear All | 2. Clear completed: > "))
+                match(option):
                     case 1:
-                        text = input("Todo: > ")
-                        priority = int(input("Priority 1-3 (default 3): > "))
-                        if priority > 3 or priority < 0:
-                            priority = 3
-                        todo_list.append(Todo(priority, text))
-                    case 2:
-                        index = int(input("Todo number: > "))
-                        if index > len(todo_list):
-                            continue
-                        todo_list[index - 1].completed = True
-                    case 3:
-                        index = int(input("Todo number: > "))
-                        if index > len(todo_list):
-                            continue
-                        del todo_list[int(index) - 1]
-                    case 4:
                         todo_list = []
-                    case 5:
+                    case 2:
                         todo_list = list(filter(lambda x: not x.completed, todo_list))
-                    case 6:
-                        def filterer(todo: Todo) -> int | float:
-                            if todo.completed:
-                                return float("inf")
-                            else:
-                                return todo.priority
-                        todo_list.sort(key=filterer)
-                    case _:
+            #Check for existing input
+            elif user_input != "":
+                #Check that is a valid todo
+                try:
+                    if int(user_input) > 0 and int(user_input) <= len(todo_list):
+                        index = int(user_input)
+                        option = int(input("1. Complete Todo | 2. Delete Todo | 3. Edit Todo: > "))
+                        #Check for valid options
+                        match(option):
+                            case 1:
+                                todo_list[index - 1].completed = True
+                            case 2:
+                                del todo_list[index - 1]
+                            case 3:
+                                new_text = input(f"New description (old '{todo_list[index - 1].text})' > ")
+                                # Check that priority is correct
+                                while True:
+                                    new_priority = int(input(f"New priority (old '{todo_list[index - 1].priority}') > "))
+                                    if new_priority > 0 and new_priority <= 3:
+                                        break
+                                #update todo
+                                todo_list[index - 1].text = new_text
+                                todo_list[index - 1].priority = new_priority
                         continue
+                except ValueError:
+                    print("Adding new Todo:")
+                text = user_input
+                while True:
+                    priority = int(input("Priority: 1-3: > "))
+                    if priority > 0 and priority <= 3:
+                        break
+                todo_list.append(Todo(priority, text))
             save_todo_list(todo_list)
-        except ValueError:
-            continue
         except KeyboardInterrupt:
             return 1
 main()
